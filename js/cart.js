6cf4d4062617364f650897cff9e52d85c899da09
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-10 22:57:15 +0600

    cart

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,25 @@ const handleAddProducts=()=>{
     const quantityEl=document.getElementById('quantity')
     const products=productEl.value
     const quantity=quantityEl.value
-    console.log('product Name',",", products,"Quantity :", quantity);
+    // console.log('Product Name:',"",products,",", "Quantity:", quantity);
     productEl.value=""
     quantityEl.value=""
+displayProducts(products, quantity)
+addProductsToCart(products,quantity)
+}
+const getCart=()=>{
+const cart={}
+return cart
+}
+const addProductsToCart=(products, quantity)=>{
+    const cart=getCart()
+    cart[products]=quantity
+    console.log('cart',cart)
+}
+
+const displayProducts=(products, quantity)=>{
+const li=document.createElement('li')
+li.innerText=`${products} ${quantity}`
+const productsContainer=document.getElementById('products-container')
+productsContainer.append(li)
 }
